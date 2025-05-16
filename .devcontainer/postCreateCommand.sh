fb77c419a05928df03b89bf3dfb499441e9d3a9e
Jun Ohtani <johtani@gmail.com>
2025-05-16 09:19:26 +0900

    Initial commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# postCreateCommand.sh
+
+echo "START Install"
+sudo chown -R vscode:vscode .
+
+# setup python env
+poetry config virtualenvs.in-project true
+poetry run pip install pip poetry wheel setuptools 
+
+VENV=`poetry env info -p`
+echo "source ${VENV}/bin/activate" >> ~/.bashrc
+
+source ~/.bashrc
+
+
+echo "FINISH Install"
